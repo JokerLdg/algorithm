c2ff0a0146853e6aec0131a54fd666a560d6a6e3
imdong-geun <dg600123@gmail.com>
2022-01-15 16:37:18 +0900

    read me 수정

---
+++ Baekjoon/greedy/1439.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+#define endl '\n'
+
+int main() {
+    // 뒤집기
+    // cin, cout 최적화
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+
+
+    return 0;
+}
